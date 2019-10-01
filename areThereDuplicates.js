039a9400d825f58c99d1c0517af08dbef53421e7
Carly Pecora <carly@carlypecora.com>
2019-10-01 11:47:54 -0400

    add multiple solutions

--- areThereDuplicates.js
+++ areThereDuplicates.js
@@ -24,6 +24,43 @@ function areThereDuplicates(...args) {
     return false
 }
 
+//OR
+
+// function areThereDuplicates() {
+//   let collection = {}
+//   for(let val in arguments){
+//     collection[arguments[val]] = (collection[arguments[val]] || 0) + 1
+//   }
+//   for(let key in collection){
+//     if(collection[key] > 1) return true
+//   }
+//   return false;
+// }
+
+//OR
+
+// function areThereDuplicates(...args) {
+//   // Two pointers
+//   args.sort((a,b) => a > b);
+//   let start = 0;
+//   let next = 1;
+//   while(next < args.length){
+//     if(args[start] === args[next]){
+//         return true
+//     }
+//     start++
+//     next++
+//   }
+//   return false
+// }
+
+//OR
+
+// function areThereDuplicates() {
+	//each value in set MUST be unique
+//   return new Set(arguments).size !== arguments.length;
+// }
+
 console.log(areThereDuplicates(1,2,3))
 console.log(areThereDuplicates(10, 45, 67, 45))
 console.log(areThereDuplicates('f', 'g', 'f', 'd'))
